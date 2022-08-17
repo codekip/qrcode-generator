264c0b85b06f3e7d1bfbfdac6f28e5df4f90528e
Nick <codekip@outlook.com>
2022-08-17 09:04:18 +0100

    Added inputbox for initiative name that shows at the top of the pdf

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let btn_print = document.getElementById("btn-print");
 let btn_clear = document.getElementById("btn-clear");
 let qr_code = document.getElementById("qr-code");
 let file_url = document.getElementById("file-url");
+let initiative_name = document.getElementById("initiative-name");
 
 let qrcode = undefined;
 
@@ -25,18 +26,21 @@ function generate_qrcode(value) {
   }
 }
 
-
 function clear_qrcode() {
   qrcode = undefined;
   qr_code.innerHTML = "";
   file_url.value = "";
 }
 
+function clear_initiative_name() {
+  initiative_name.value = "";
+}
+
 function pdf() {
   var pdf = new jsPDF();
 
   pdf.setFontSize(15);
-  pdf.text("Sampling initiative: ", 25, 20);
+  pdf.text("Sampling initiative: " + initiative_name.value, 25, 20);
 
   pdf.setFontSize(12);
   pdf.text("Scan the QR code above to open the file", 66, 120);
